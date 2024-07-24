7b5660000a73e88983db58135aa2439efcbf16f0
Tolga <Tolga@DESKTOP-N1O1CS1>
2024-07-24 13:31:00 +0300

    Add project files.

---
+++ ResAPI/Reservation.API.Services/ReservationService.cs
@@ -0,0 +1,24 @@
+﻿using Reservation.API.Infrastructure;
+using Reservation.API.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Reservation.API.Services
+{
+    public class ReservationService : IReservationService
+    {
+        public ReservationDTO GetReservationByBookingNumber(int bookingNumber)
+        {
+            return new ReservationDTO()
+            {
+                Id = (new Random()).Next(100),
+                BookingNumber = bookingNumber,
+                Amount = (new Random().Next(10000)),
+                BookingDate = DateTime.Now,
+                CheckinDate = DateTime.Now.Date.AddDays(30),
+                CheckOutDate = DateTime.Now.Date.AddDays(37)
+            };
+        }
+    }
+}
---
+++ ResAPI/Reservation.API/Controllers/ReservationController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Reservation.API.Infrastructure;
+using Reservation.API.Models;
+
+namespace Reservation.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ReservationController : ControllerBase
+    {
+        private readonly IReservationService reservationService;
+        public ReservationController(IReservationService _reservationService)
+        {
+            reservationService = _reservationService;
+        }
+
+        [HttpGet("{Id}")]
+        public ReservationDTO Get(int Id)
+        {
+            return reservationService.GetReservationByBookingNumber(Id);
+        }
+    }
+}
